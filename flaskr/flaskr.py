a48b45b98fe095fcb8a4116eeb78639c907c6e9f
Daniel Pipkin <drpipkin@gmail.com>
2017-04-06 12:37:35 -0600

    :bug: Fix login session

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -77,7 +77,7 @@ def login():
         elif request.form['password'] != app.config['PASSWORD']:
             error = 'Invalid password'
         else:
-            session['logged_ing'] = True
+            session['logged_in'] = True
             flash('You were logged in')
             return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
